c9a417bd9037f6f6113bb3bb946974dd60547b12
Nathan <savardnm@gmail.com>
2023-11-01 16:18:58 +0900

    alter main to create and display fields
    created task objects and widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,135 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+class Task {
+  final String id;
+  final String name;
+  final String description;
+  final String parentId;
+  final List<TaskField> fields;
+
+  Task({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.parentId,
+    required this.fields,
+  });
 }
+
+class TaskField {
+  final String name;
+  final String value;
+
+  TaskField({
+    required this.name,
+    required this.value,
+  });
+}
+
+void main() {
+  // Create tasks with fields
+  final task1 = Task(
+    id: '1',
+    name: 'Task 1',
+    description: 'Description for Task 1',
+    parentId: '0',
+    fields: [
+      TaskField(name: 'Field1', value: 'Value1'),
+      TaskField(name: 'Field2', value: 'Value2'),
+    ],
+  );
+
+  final task2 = Task(
+    id: '2',
+    name: 'Task 2',
+    description: 'Description for Task 2',
+    parentId: '1',
+    fields: [
+      TaskField(name: 'Field1', value: 'Value1'),
+      TaskField(name: 'Field3', value: 'Value3'),
+    ],
+  );
+
+  // Display task information
+  print('Task 1:');
+  print('ID: ${task1.id}');
+  print('Name: ${task1.name}');
+  print('Description: ${task1.description}');
+  print('Parent ID: ${task1.parentId}');
+  print('Fields:');
+  for (var field in task1.fields) {
+    print('${field.name}: ${field.value}');
+  }
+
+  print('\nTask 2:');
+  print('ID: ${task2.id}');
+  print('Name: ${task2.name}');
+  print('Description: ${task2.description}');
+  print('Parent ID: ${task2.parentId}');
+  print('Fields:');
+  for (var field in task2.fields) {
+    print('${field.name}: ${field.value}');
+  }
+  runApp(MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Replace this with your list of tasks
+    final List<Task> tasks = [
+      Task(id: '1', name: 'Task 1', description: 'Description for Task 1', parentId: '0', fields: [
+        TaskField(name: 'Field1', value: 'Value1'),
+        TaskField(name: 'Field3', value: 'Value3'),
+      ]),
+      Task(id: '2', name: 'Task 2', description: 'Description for Task 2', parentId: '1', fields: []),
+    ];
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Task List'),
+        ),
+        body: TaskList(tasks: tasks),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class TaskList extends StatelessWidget {
+  final List<Task> tasks;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  TaskList({required this.tasks});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        final task = tasks[index];
+        return TaskCard(task: task);
+      },
     );
   }
 }
+
+class TaskCard extends StatelessWidget {
+  final Task task;
+
+  TaskCard({required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: ListTile(
+        title: Text(task.name),
+        subtitle: Text(task.description),
+        // You can add more widgets here to display additional task information
+      ),
+    );
+  }
+}
+
+
