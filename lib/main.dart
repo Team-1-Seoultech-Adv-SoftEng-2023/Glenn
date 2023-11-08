e670110637a745e6c0bc8695c06bcd6e36f78c52
anjaburecke31 <40232971+anjabruecke31@users.noreply.github.com>
2023-11-08 19:09:14 +0900

    due date task

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class Task {
 
 class TaskField {
   final String name;
-  final String value;
+  String value;
 
   TaskField({
     required this.name,
@@ -29,50 +29,95 @@ class TaskField {
   });
 }
 
+class DueDateField extends TaskField {
+  DateTime _dueDate;
+  TimeOfDay _dueTime;
+
+  DueDateField({
+    required DateTime dueDate,
+    required TimeOfDay dueTime,
+  })   : _dueDate = dueDate,
+        _dueTime = dueTime,
+        super(name: 'Due Date', value: '${_formatDate(dueDate)} ${_formatTime(dueTime)}');
+
+  DateTime get dueDate => _dueDate;
+  set dueDate(DateTime value) {
+    _dueDate = value;
+    updateValue();
+  }
+
+  TimeOfDay get dueTime => _dueTime;
+  set dueTime(TimeOfDay value) {
+    _dueTime = value;
+    updateValue();
+  }
+
+  void updateValue() {
+    super.value = '${_formatDate(_dueDate)} ${_formatTime(_dueTime)}';
+  }
+
+  static String _formatDate(DateTime date) {
+    return '${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
+  }
+
+  static String _formatTime(TimeOfDay time) {
+    return '${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}';
+  }
+}
+
+
 void main() {
-  // Create tasks with fields
-  final task1 = Task(
-    id: '1',
-    name: 'Task 1',
-    description: 'Description for Task 1',
-    parentId: '0',
-    fields: [
-      TaskField(name: 'Field1', value: 'Value1'),
-      TaskField(name: 'Field2', value: 'Value2'),
-    ],
-  );
+  // // Create tasks with fields
+  // final task1 = Task(
+  //   id: '1',
+  //   name: 'Task 1',
+  //   description: 'Description for Task 1',
+  //   parentId: '0',
+  //   fields: [
+  //     DueDateField(
+  //       dueDate: DateTime(2023, 11, 8),
+  //       dueTime: TimeOfDay(hour: 12, minute: 0),
+  //     ),
+  //     TaskField(name: 'Field1', value: 'Value1'),
+  //     TaskField(name: 'Field2', value: 'Value2'),
+  //   ],
+  // );
 
-  final task2 = Task(
-    id: '2',
-    name: 'Task 2',
-    description: 'Description for Task 2',
-    parentId: '1',
-    fields: [
-      TaskField(name: 'Field1', value: 'Value1'),
-      TaskField(name: 'Field3', value: 'Value3'),
-    ],
-  );
+  // final task2 = Task(
+  //   id: '2',
+  //   name: 'Task 2',
+  //   description: 'Description for Task 2',
+  //   parentId: '1',
+  //   fields: [
+  //     DueDateField(
+  //       dueDate: DateTime(2023, 11, 10),
+  //       dueTime: TimeOfDay(hour: 15, minute: 30),
+  //     ),
+  //     TaskField(name: 'Field1', value: 'Value1'),
+  //     TaskField(name: 'Field3', value: 'Value3'),
+  //   ],
+  // );
 
-  // Display task information
-  print('Task 1:');
-  print('ID: ${task1.id}');
-  print('Name: ${task1.name}');
-  print('Description: ${task1.description}');
-  print('Parent ID: ${task1.parentId}');
-  print('Fields:');
-  for (var field in task1.fields) {
-    print('${field.name}: ${field.value}');
-  }
+  // // Display task information
+  // print('Task 1:');
+  // print('ID: ${task1.id}');
+  // print('Name: ${task1.name}');
+  // print('Description: ${task1.description}');
+  // print('Parent ID: ${task1.parentId}');
+  // print('Fields:');
+  // for (var field in task1.fields) {
+  //   print('${field.name}: ${field.value}');
+  // }
 
-  print('\nTask 2:');
-  print('ID: ${task2.id}');
-  print('Name: ${task2.name}');
-  print('Description: ${task2.description}');
-  print('Parent ID: ${task2.parentId}');
-  print('Fields:');
-  for (var field in task2.fields) {
-    print('${field.name}: ${field.value}');
-  }
+  // print('\nTask 2:');
+  // print('ID: ${task2.id}');
+  // print('Name: ${task2.name}');
+  // print('Description: ${task2.description}');
+  // print('Parent ID: ${task2.parentId}');
+  // print('Fields:');
+  // for (var field in task2.fields) {
+  //   print('${field.name}: ${field.value}');
+  // }
   runApp(MyApp());
 }
 
@@ -82,9 +127,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // Replace this with your list of tasks
     final List<Task> tasks = [
-      Task(id: '1', name: 'Task 1', description: 'Description for Task 1', parentId: '0', fields: [
-        TaskField(name: 'Field1', value: 'Value1'),
-        TaskField(name: 'Field3', value: 'Value3'),
+      Task(
+        id: '1', 
+        name: 'Task 1', 
+        description: 'Description for Task 1', 
+        parentId: '0', 
+        fields: [
+          DueDateField(
+            dueDate: DateTime(2023, 11, 10),
+            dueTime: TimeOfDay(hour: 15, minute: 30),
+          ),
+          TaskField(name: 'Field1', value: 'Value1'),
+          TaskField(name: 'Field3', value: 'Value3'),
       ]),
       Task(id: '2', name: 'Task 2', description: 'Description for Task 2', parentId: '1', fields: []),
     ];
@@ -117,10 +171,31 @@ class TaskList extends StatelessWidget {
   }
 }
 
-class TaskCard extends StatelessWidget {
+class TaskCard extends StatefulWidget {
   final Task task;
 
   TaskCard({required this.task});
+
+  @override
+  _TaskCardState createState() => _TaskCardState();
+}
+
+class _TaskCardState extends State<TaskCard> {
+  late TextEditingController dateController;
+  late TextEditingController timeController;
+
+  @override
+  void initState() {
+    super.initState();
+    dateController = TextEditingController(
+        text: widget.task.fields.isNotEmpty
+            ? _formatDate((widget.task.fields.first as DueDateField).dueDate)
+            : '');
+    timeController = TextEditingController(
+        text: widget.task.fields.isNotEmpty
+            ? _formatTime((widget.task.fields.first as DueDateField).dueTime)
+            : '');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -130,27 +205,79 @@ class TaskCard extends StatelessWidget {
         onTap: () {
           Navigator.of(context).push(
             MaterialPageRoute(
-              builder: (context) => TaskDetailPage(task: task),
+              builder: (context) => TaskDetailPage(task: widget.task),
             ),
           );
         },
         child: Column(
           children: <Widget>[
             ListTile(
-              title: Text(task.name),
-              subtitle: Text(task.description),
+              title: Text(widget.task.name),
+              subtitle: Text(widget.task.description),
             ),
-            if (task.fields.isNotEmpty)
+            if (widget.task.fields.isNotEmpty)
               Container(
                 padding: EdgeInsets.all(10),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: task.fields.map((field) {
-                    return ListTile(
-                      title: Text(field.name),
-                      subtitle: Text(field.value),
-                    );
-                  }).toList(),
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('Due Date'),
+                      subtitle: Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              controller: dateController,
+                              keyboardType: TextInputType.datetime,
+                              decoration: InputDecoration(labelText: 'Date'),
+                              onTap: () async {
+                                DateTime? selectedDate = await showDatePicker(
+                                  context: context,
+                                  initialDate: (widget.task.fields.first as DueDateField).dueDate,
+                                  firstDate: DateTime(2000),
+                                  lastDate: DateTime(2101),
+                                );
+                                if (selectedDate != null) {
+                                  setState(() {
+                                    (widget.task.fields.first as DueDateField).dueDate = selectedDate;
+                                    dateController.text = _formatDate(selectedDate);
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                          SizedBox(width: 16),
+                          Expanded(
+                            child: TextFormField(
+                              controller: timeController,
+                              keyboardType: TextInputType.datetime,
+                              decoration: InputDecoration(labelText: 'Time'),
+                              onTap: () async {
+                                TimeOfDay? selectedTime = await showTimePicker(
+                                  context: context,
+                                  initialTime: (widget.task.fields.first as DueDateField).dueTime,
+                                );
+                                if (selectedTime != null) {
+                                  setState(() {
+                                    (widget.task.fields.first as DueDateField).dueTime = selectedTime;
+                                    timeController.text = _formatTime(selectedTime);
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('Field1'),
+                      subtitle: Text('Value1'),
+                    ),
+                    ListTile(
+                      title: Text('Field3'),
+                      subtitle: Text('Value3'),
+                    ),
+                  ],
                 ),
               ),
           ],
@@ -158,7 +285,12 @@ class TaskCard extends StatelessWidget {
       ),
     );
   }
+
+  String _formatDate(DateTime date) {
+    return '${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
+  }
+
+  String _formatTime(TimeOfDay time) {
+    return '${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}';
+  }
 }
-
-
-
